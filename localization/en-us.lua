085c6642337643d8c41c02ef46a7643a2adc5747
Eremel <richardbailey@outlook.com>
2025-11-29 13:29:12 +0000

    Adds badge support `{stocking}before{}` and `{stocking}after{}`

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -13,7 +13,9 @@ return {
             stocking_stuffer_to_jokers = 'Show Jokers',
             stocking_stuffer_to_presents = 'Show Presents',
             stocking_stuffer_present_count = 'Presents: ',
-            stocking_stuffer_under_the_tree = 'Under the tree'
+            stocking_stuffer_under_the_tree = 'Under the tree',
+            stocking_stuffer_before = 'before',
+            stocking_stuffer_after = 'after'
         }
     },
     descriptions = {
--- localization/en-us/santa.lua
+++ localization/en-us/santa.lua
@@ -4,8 +4,10 @@ return {
             ["Santa Claus_stocking_snowglobe"] = {
                 name = 'Snow Globe',
                 text = {
-                    '{C:mult}+#1#{} Mult before',
-                    '{C:chips}#2#{} Chips after'
+                    '{stocking}before{}',
+                    '{C:mult}+#1#{} Mult',
+                    '{stocking}after{}',
+                    '{C:chips}#2#{} Chips',
                 }
             },
             ["Santa Claus_stocking_toy_train"] = {
