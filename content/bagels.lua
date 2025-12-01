fbfc3c0d7eba69567523286dfe1f6bae735b1fcf
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2025-12-01 13:15:23 -0500

    Implement Gift Receipt

---
+++ content/bagels.lua
@@ -0,0 +1,80 @@
+local display_name = 'BakersDozenBagels'
+
+SMODS.Atlas {
+    key = display_name .. '_presents',
+    path = 'bagels_presents.png',
+    px = 71,
+    py = 95
+}
+
+StockingStuffer.Developer {
+    name = display_name,
+    colour = HEX 'EDD198'
+}
+
+StockingStuffer.WrappedPresent {
+    developer = display_name,
+    pos = { x = 1, y = 0 },
+    atlas = 'presents' -- TODO
+}
+
+--#region Gift Receipt
+
+StockingStuffer.Present {
+    developer = display_name,
+
+    key = 'GiftReceipt',
+    pos = { x = 1, y = 0 },
+
+    config = { extra = 3 },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra } }
+    end,
+
+    -- This retriggers 2s, 3s, 4s, and 5s, but I can't think of something better
+    update = function(self)
+        for _, c in pairs(G.jokers.cards) do
+            c:set_cost()
+        end
+        for _, c in pairs(G.consumeables.cards) do
+            c:set_cost()
+        end
+    end
+}
+
+local raw_Card_set_cost = Card.set_cost
+function Card:set_cost(...)
+    raw_Card_set_cost(self, ...)
+    if not G.stocking_present or self.stocking_was_bought then return end
+    for _, c in pairs(G.stocking_present.cards) do
+        if c.config.center.key == 'BakersDozenBagels_stocking_GiftReceipt' then
+            self.sell_cost = self.sell_cost + c.ability.extra
+            self.sell_cost_label = self.facing == 'back' and '?' or self.sell_cost
+        end
+    end
+end
+
+local raw_G_FUNCS_buy_from_shop = G.FUNCS.buy_from_shop
+function G.FUNCS.buy_from_shop(e)
+    raw_G_FUNCS_buy_from_shop(e)
+    local card = e.config.ref_table
+    if card and card:is(Card) then
+        card.stocking_was_bought = true
+        card:set_cost()
+    end
+end
+
+local raw_Card_save = Card.save
+function Card:save()
+    local ret = raw_Card_save(self)
+    ret.stocking_was_bought = self.stocking_was_bought
+    return ret
+end
+
+local raw_Card_load = Card.load
+function Card:load(table, other)
+    raw_Card_load(self, table, other)
+    self.stocking_was_bought = table.stocking_was_bought
+end
+
+--#endregion
---
+++ localization/en-us/BakersDozenBagels.lua
@@ -0,0 +1,23 @@
+return {
+    descriptions = {
+        stocking_present = {
+            BakersDozenBagels_stocking_GiftReceipt = {
+                name = 'Gift Receipt',
+                text = {
+                    '{C:attention}Jokers{} and {C:attention}Consumables',
+                    'sell for {C:money}$#1#{} more',
+                    "if they {C:red}weren't bought"
+                }
+            }
+        },
+        -- stocking_wrapped_present = {
+        --     template_stocking_present = {
+        --         name = '{V:1}Present',
+        --         text = {
+        --             '  {C:inactive}What could be inside?  ',
+        --             '{C:inactive}Open me to find out!'
+        --         }
+        --     },
+        -- }
+    }
+}
