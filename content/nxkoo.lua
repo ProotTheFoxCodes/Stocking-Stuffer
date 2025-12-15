c30a583e2e84994c07aab27b4c650472009a2124
Clicky- <84652396+Clickseee@users.noreply.github.com>
2025-12-15 09:37:02 +0700

    WE UP FELLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASS

---
+++ content/nxkoo.lua
@@ -0,0 +1,204 @@
+-- HUGE shoutout to Somethingcom525 for helping me code this
+local oldsetcost = Card.set_cost
+function Card:set_cost()
+    local g = oldsetcost(self)
+    if next(SMODS.find_card("Nxkoo_stocking_dealmaker")) then
+        self.cost = pseudorandom('dealmaker_cost_' .. self.sort_id, 0.001,
+            100)
+    end
+    return g
+end
+
+local cost_dt = 0
+local oldgameupdate = Game.update
+function Game:update(dt)
+    local g = oldgameupdate(self, dt)
+    if G.shop and G.jokers and next(SMODS.find_card("Nxkoo_stocking_dealmaker")) then
+        cost_dt = cost_dt + dt
+        if cost_dt > 0.2 then
+            cost_dt = cost_dt - 0.2
+            for i, v in ipairs({ G.shop_jokers, G.shop_vouchers, G.shop_booster }) do
+                for ii, vv in ipairs(v.cards) do
+                    vv:set_cost()
+                end
+            end
+        end
+    end
+    return g
+end
+
+SMODS.Font {
+    key = "DETERMINATION",
+    path = "determination.ttf",
+    render_scale = 200,
+    TEXT_HEIGHT_SCALE = 0.75,
+    TEXT_OFFSET = { x = 10, y = -17 },
+    FONTSCALE = 0.075,
+    squish = 1,
+    DESCSCALE = 1
+}
+
+local display_name = 'Nxkoo'
+
+SMODS.Atlas({
+    key = display_name .. '_presents',
+    path = 'Nxkoo_presents.png',
+    px = 71,
+    py = 95
+})
+
+
+-- Linkage of Wrapped & Presents
+StockingStuffer.Developer({
+    name = display_name, -- DO NOT CHANGE
+    colour = HEX('780606')
+})
+
+StockingStuffer.WrappedPresent({
+    developer = display_name, -- DO NOT CHANGE
+    artist = { "missingnumber" },
+    coder = { "Nxkoo" },
+    pos = { x = 0, y = 0 },
+})
+
+StockingStuffer.Present({
+    developer = display_name,
+    key = 'gun',
+    pos = { x = 1, y = 0 },
+    config = { extra = { odds = 4 } },
+    artist = { "missingnumber" },
+    coder = { "notmario" },
+    blueprint_compat = false,
+    loc_vars = function(self, info_queue, card)
+        local new_numerator, new_denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'whatever')
+        return { vars = { new_numerator, new_denominator, card.ability.extra.uses } }
+    end,
+    can_use = function(self, card)
+        local right_joker = nil
+        for i = 1, #G.stocking_present.cards do
+            if G.stocking_present.cards[i] == card then
+                right_joker = G.stocking_present.cards[i + 1]
+            end
+        end
+        return right_joker ~= nil
+    end,
+    use = function(self, card)
+        if not SMODS.pseudorandom_probability(card, 'brah', 1, card.ability.extra.odds) then
+            local right_joker = nil
+            for i = 1, #G.stocking_present.cards do
+                if G.stocking_present.cards[i] == card then
+                    right_joker = G.stocking_present.cards[i + 1]
+                end
+            end
+            if right_joker then
+                SMODS.destroy_cards(right_joker)
+            end
+        else
+            if G.STAGE == G.STAGES.RUN then
+                G.STATE = G.STATES.GAME_OVER; G.STATE_COMPLETE = false
+            end
+        end
+    end,
+    keep_on_use = function(self, card)
+        return true
+    end
+})
+
+StockingStuffer.Present({
+    developer = display_name,
+    key = 'numerosloppen',
+    pos = { x = 2, y = 0 },
+    artist = { "missingnumber" },
+    coder = { "Nxkoo" },
+    blueprint_compat = false,
+    config = {},
+    calculate = function(self, card, context)
+        if context.joker_main and StockingStuffer.second_calculation then
+            local spades = 0
+            for _, c in ipairs(G.hand.cards) do
+                if c:is_suit('Spades', nil, true) then
+                    spades = spades + 1
+                end
+            end
+
+            if spades > 0 then
+                if Talisman and Big and Big.arrow then
+                    return {
+                        eemult = 2 ^ spades
+                    }
+                else
+                    return {
+                        func = function()
+                            for _ = 1, spades do
+                                mult = mod_mult(mult ^ 2)
+                            end
+                        end,
+                        message = "^^" .. spades .. " Mult",
+                        colour = G.C.DARK_EDITION
+                    }
+                end
+            end
+        end
+    end
+})
+
+StockingStuffer.Present({
+    developer = display_name,
+    key = 'monstercan',
+    pos = { x = 3, y = 0 },
+    artist = { "lord.ruby" },
+    coder = { "Nxkoo" },
+    blueprint_compat = false,
+    use = function(self, card)
+        for _, c in ipairs(G.playing_cards) do
+            SMODS.change_base(c, c.base.suit, "Queen")
+        end
+        card.ability.used = true
+    end,
+    can_use = function(self, card)
+        return G.GAME.blind.in_blind and not card.ability.used
+    end,
+    calculate = function(self, card, context)
+        if context.end_of_round then
+            card.ability.used = nil
+        end
+    end,
+    keep_on_use = function(self, card)
+        return true
+    end
+})
+
+StockingStuffer.Present({                       -- QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+    developer = display_name,                   -- QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+    key = 'dealmaker',                          -- QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+    pos = { x = 4, y = 0 },                     -- QQQQQQQQQQQQQQQQQQQWQQQQQWWWBBBHHHHHHHHHBWWWQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+    artist = { "lord.ruby" },                   -- QQQQQQQQQQQQQQQQD!`__ssaaaaaaaaaass_ass_s____.  -~""??9VWQQQQQQQQQQQQQQQQQQQ
+    coder = { "Nxkoo" },                        -- QQQQQQQQQQQQQQP'_wmQQQWWBWV?GwwwmmWQmwwwwwgmZUVVHAqwaaaac,"?9$QQQQQQQQQQQQQQ
+    blueprint_compat = false,                   -- QQQQQQQQQQQW! aQWQQQQW?qw#TTSgwawwggywawwpY?T?TYTYTXmwwgZ$ma/-?4QQQQQQQQQQQ
+    loc_vars = function(self, info_queue, card) -- QQQQQQQQQQQW' jQQQQWTqwDYauT9mmwwawww?WWWWQQQQQ@TT?TVTT9HQQQQQQw,-4QQQQQQQQQ
+    end,                                        -- QQQQQQQQQQQ[ jQQQQQyWVw2$wWWQQQWWQWWWW7WQQQQQQQQPWWQQQWQQw7WQQQWWc)WWQQQQQQQ
+    config = {},                                -- QQQQQQQQQQf jQQQQQWWmWmmQWU???????9WWQmWQQQQQQQWjWQQQQQQQWQmQQQQWL 4QQQQQQQQ
+    calculate = function(self, card, context)   -- QQQQQQQQP'.yQQQQQQQQQQQP"       <wa,.!4WQQQQQQQWdWP??!"??4WWQQQWQQc ?QWQQQQQ
+    end                                         -- QQQQQP'_a.<aamQQQW!<yF "!` ..  "??$Qa "WQQQWTVP'    "??' =QQmWWV?46/ ?QQQQQ
+})                                              -- QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQWQ
+
+StockingStuffer.Present({
+    developer = display_name,
+    key = 'ps5',
+    pos = { x = 6, y = 0 },
+    artist = { "missingno" },
+    coder = { "Nxkoo" },
+    config = { extra = { percent = -50 } },
+    blueprint_compat = false,
+    add_to_deck = function(self, card, from_debuff)
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                G.GAME.discount_percent = card.ability.extra.percent
+                for _, v in pairs(G.I.CARD) do
+                    if v.set_cost then v:set_cost() end
+                end
+                return true
+            end
+        }))
+    end,
+})
---
+++ localization/en-us/nxkoo.lua
@@ -0,0 +1,87 @@
+return {
+    descriptions = {
+        stocking_present = {
+            Nxkoo_stocking_gun = {
+                name = {
+                    'A Gun',
+                    '{s:0.8}(TOTALLY LEGAL)'
+                },
+                text = {
+                    {
+                    'Use this to destroy',
+                    '{C:stocking_present}Presents{} to the right',
+                    '{stocking}usable{}'
+                    },
+                    {
+                        "{C:green}#1# in #2#{} chance to",
+                        "shoot {C:red}yourself{} instead"
+                    }
+                }
+            },
+            Nxkoo_stocking_numerosloppen = {
+                name = {
+                    'A Can of Slop',
+                    '{s:0.8}(Imported from Cryptville)'
+                },
+                text = {
+                    {
+                    '{X:dark_edition,C:white}^^2{} Mult for',
+                    'each {C:spades}Spades{} card {C:attention}held in hand',
+                    '{stocking}after{}'
+                    }
+                }
+            },
+                Nxkoo_stocking_monstercan = {
+                name = {
+                    'A Can of White Monster',
+                    '{s:0.8}(Stolen from Scalpers)'
+                },
+                text = {
+                    {
+                    'Use this {C:stocking_present}Present{} to',
+                    'change the rank of',
+                    'all {C:attention}selected{} cards',
+                    'into {C:attention}Queens{}',
+                    '{s:0.8}(Only works once per round)',
+                    '{stocking}usable{}'
+                    }
+                }
+            },
+                Nxkoo_stocking_dealmaker = {
+                name = {
+                    '{f:stocking_DETERMINATION}DEALMAKER{}',
+                    '{s:0.8}($$$$)'
+                },
+                text = {
+                    {
+                        "SHUFFLES THE {X:money,C:white}[[PRICE TAGS]]{}",
+                        "OF EVERY {X:attention,C:white}[[CARD]]{}",
+                        "IN THE {X:attention,C:white}[[SHOP ZONE]]{!!"
+                    }
+                }
+            },
+                Nxkoo_stocking_ps5 = {
+                name = {
+                    'PolyChromation 5',
+                    '{s:0.8}("Brand New")'
+                },
+                text = {
+                    {
+                        "Unfortunately, it has no game..",
+                        "{C:inactive}Does Nothing?..{}"
+                    }
+                }
+            },
+        },
+        stocking_wrapped_present = {
+            Nxkoo_stocking_present = {
+                name = '"Present"',
+                text = {
+                    "..Trust me, it's not what",
+                    "you think it is.",
+                    "{C:inactive,s:0.8}(Merry Christmas!)"
+                }
+            },
+        }
+    }
+}
